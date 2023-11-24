3491c2dd46f8970f372c651ce4211c57de3542c1
chenjie <chenjie@hongwuniu.com>
2023-11-24 11:05:04 +0800

    增加CompletableFuture的测试DEMO

--- JUC/src/main/java/com/chenjie/juc/Thread/TestCompletableFuture.java
+++ JUC/src/main/java/com/chenjie/juc/Thread/TestCompletableFuture.java
@@ -430,7 +430,7 @@ public class TestCompletableFuture {
      */
     @Test
     public void testCompletableFuture_Async_Combination2() throws ExecutionException, InterruptedException {
-// 创建异步执行任务:
+        // 创建异步执行任务:
         CompletableFuture<Double> job = CompletableFuture.supplyAsync(() -> {
             System.out.println(Thread.currentThread() + " start job1,time->" + System.currentTimeMillis());
             try {
@@ -487,6 +487,104 @@ public class TestCompletableFuture {
         System.out.println("job5 run result->" + job5.get());
         System.out.println("main thread exit,time->" + System.currentTimeMillis());
     }
+
+
+    /**
+     * thenCompose互相依赖
+     * 两个任务之间有前后依赖关系，但是连接任务又是独立的CompletableFuture
+     */
+    @Test
+    public void testCompletableFuture_Async_Combination3() throws ExecutionException, InterruptedException {
+        // 创建异步执行任务:
+        CompletableFuture<Double> job = CompletableFuture.supplyAsync(() -> {
+            System.out.println(Thread.currentThread() + " start job1,time->" + System.currentTimeMillis());
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException ignored) {
+            }
+            System.out.println(Thread.currentThread() + " exit job1,time->" + System.currentTimeMillis());
+            return 1.2;
+        });
+        CompletableFuture<String> job2 = job.thenCompose((param) -> {
+            System.out.println(Thread.currentThread() + " start job2,time->" + System.currentTimeMillis());
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException ignored) {
+            }
+            System.out.println(Thread.currentThread() + " exit job2,time->" + System.currentTimeMillis());
+            return CompletableFuture.supplyAsync(() -> {
+                System.out.println(Thread.currentThread() + " start job3,time->" + System.currentTimeMillis());
+                try {
+                    Thread.sleep(2000);
+                } catch (InterruptedException ignored) {
+                }
+                System.out.println(Thread.currentThread() + " exit job3,time->" + System.currentTimeMillis());
+                return "job3 test";
+            });
+        });
+        System.out.println("main thread start job.get(),time->" + System.currentTimeMillis());
+        //等待子任务执行完成
+        System.out.println("job run result->" + job.get());
+        System.out.println("main thread start job2.get(),time->" + System.currentTimeMillis());
+        System.out.println("job2 run result->" + job2.get());
+        System.out.println("main thread exit,time->" + System.currentTimeMillis());
+    }
+
+
+    /**
+     * allOf / anyOf
+     * <p>
+     * allOf返回的CompletableFuture是多个任务都执行完成后才会执行，只要有一个任务执行异常，则返回的CompletableFuture执行get方法时会抛出异常，如果都是正常执行，则get返回null。
+     * <p>
+     * anyOf返回的CompletableFuture是多个任务只要其中一个执行完成就会执行，其get返回的是已经执行完成的任务的执行结果，如果该任务执行异常，则抛出异常。
+     */
+    @Test
+    public void testCompletableFuture_Async_Combination4() throws ExecutionException, InterruptedException {
+        // 创建异步执行任务:
+        CompletableFuture<Double> job = CompletableFuture.supplyAsync(() -> {
+            System.out.println(Thread.currentThread() + " start job1,time->" + System.currentTimeMillis());
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException ignored) {
+            }
+            System.out.println(Thread.currentThread() + " exit job1,time->" + System.currentTimeMillis());
+            return 1.2;
+        });
+        CompletableFuture<Double> job2 = CompletableFuture.supplyAsync(() -> {
+            System.out.println(Thread.currentThread() + " start job2,time->" + System.currentTimeMillis());
+            try {
+                Thread.sleep(1500);
+            } catch (InterruptedException ignored) {
+            }
+            System.out.println(Thread.currentThread() + " exit job2,time->" + System.currentTimeMillis());
+            return 3.2;
+        });
+        CompletableFuture<Double> job3 = CompletableFuture.supplyAsync(() -> {
+            System.out.println(Thread.currentThread() + " start job3,time->" + System.currentTimeMillis());
+            try {
+                Thread.sleep(1300);
+            } catch (InterruptedException ignored) {
+            }
+            //            throw new RuntimeException("test");
+            System.out.println(Thread.currentThread() + " exit job3,time->" + System.currentTimeMillis());
+            return 2.2;
+        });
+        //allof等待所有任务执行完成才执行job4，如果有一个任务异常终止，则job4.get时会抛出异常，都是正常执行，job4.get返回null
+        //anyOf是只有一个任务执行完成，无论是正常执行或者执行异常，都会执行job4，job4.get的结果就是已执行完成的任务的执行结果
+        CompletableFuture job4 = CompletableFuture.allOf(job, job2, job3).whenComplete((a, b) -> {
+            if (b != null) {
+                System.out.println("error stack trace->");
+                b.printStackTrace();
+            } else {
+                System.out.println("run succ,result->" + a);
+            }
+        });
+        System.out.println("main thread start job4.get(),time->" + System.currentTimeMillis());
+        //等待子任务执行完成
+        System.out.println("job4 run result->" + job4.get());
+        System.out.println("main thread exit,time->" + System.currentTimeMillis());
+    }
+
 
     public static void main(String[] args) throws ExecutionException, InterruptedException {
         // 创建第一个异步任务，模拟耗时操作
